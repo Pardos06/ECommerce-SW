e88ead856fa3446822f79f4b8b7599db3875fb32
Roddorr2 <rodricu0703@gmail.com>
2025-09-15 18:38:38 -0500

    feat(orden): add JPA repository annotations

--- backend/src/main/java/com/ecommerce/app/orden/domain/repository/OrdenRepository.java
+++ backend/src/main/java/com/ecommerce/app/orden/domain/repository/OrdenRepository.java
@@ -1,5 +1,10 @@
 package com.ecommerce.app.orden.domain.repository;
 
-public class OrdenRepository {
+import com.ecommerce.app.orden.domain.models.Orden;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdenRepository extends JpaRepository<Integer, Orden> {
     
 }
