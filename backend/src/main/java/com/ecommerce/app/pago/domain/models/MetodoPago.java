08f6b306fe8f945f04ac78db039600be30d9c87d
Roddorr2 <rodricu0703@gmail.com>
2025-09-15 03:27:47 -0500

    feat(pago): add Jakarta validations on MetodoPago entity

--- backend/src/main/java/com/ecommerce/app/pago/domain/models/MetodoPago.java
+++ backend/src/main/java/com/ecommerce/app/pago/domain/models/MetodoPago.java
@@ -1,8 +1,41 @@
 package com.ecommerce.app.pago.domain.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class MetodoPago {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Size(max = 50)
+    @Column(length = 50, nullable = false)
+    private String nombre;
+
+    public MetodoPago() {
+    }
+
+    public MetodoPago(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
