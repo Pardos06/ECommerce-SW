edf2a606094be24ff145fe86bacab8b61cf4e5d7
Roddorr2 <rodricu0703@gmail.com>
2025-09-20 01:48:58 -0500

    feat: add authentication and authorization registration method

--- backend/src/main/java/com/ecommerce/app/interfaces/controller/AuthController.java
+++ backend/src/main/java/com/ecommerce/app/interfaces/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.app.interfaces.controller;
 
 import com.ecommerce.app.application.dto.request.AuthRequest;
+import com.ecommerce.app.application.dto.request.RegistrarClienteRequest;
 import com.ecommerce.app.application.dto.response.AuthResponse;
 import com.ecommerce.app.application.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,8 +23,13 @@ public class AuthController {
 
     @PostMapping("/login")
     @Operation(summary = "Iniciar sesión", description = "Autentica al usuario y devuelve un JWT válido")
-    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
-        AuthResponse response = authService.login(request);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    @Operation(summary = "Registrar un nuevo cliente", description = "Registra un cliente en el sistema creando su usuario y datos")
+    public ResponseEntity<AuthResponse>  registrarCliente(@RequestBody RegistrarClienteRequest request) {
+        return ResponseEntity.ok(authService.registrarCliente(request));
     }
 }
