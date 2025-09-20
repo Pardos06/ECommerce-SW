f033c0b103d5cbc849c6719c5f3f591bfc19dd07
Roddorr2 <rodricu0703@gmail.com>
2025-09-20 01:18:19 -0500

    feat: add Categoria dto, mapper, service, controller and repository method

--- backend/src/main/java/com/ecommerce/app/domain/repository/CategoriaRepository.java
+++ backend/src/main/java/com/ecommerce/app/domain/repository/CategoriaRepository.java
@@ -4,7 +4,9 @@ import com.ecommerce.app.domain.models.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-    
+    Optional<Categoria> findByNombre(String nombre);
 }
