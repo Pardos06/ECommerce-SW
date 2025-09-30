ef2b8b640d1362586c0f644ec4804ebc2d005f24
Roddorr2 <rodricu0703@gmail.com>
2025-09-30 15:30:27 -0500

    feat(interceptor): add auth interceptor to handle auth token

---
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Auth } from '../services/auth';
+
+export const authInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<any>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<any>> => {
+  const authService = inject(Auth);
+  const token = authService.obtenerToken();
+
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(cloned);
+  }
+
+  return next(req);
+};
