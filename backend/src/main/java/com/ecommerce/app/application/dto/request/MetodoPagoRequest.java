ff45014a6a83c9cac08b95c274eec0e3b566fa7f
Roddorr2 <rodricu0703@gmail.com>
2025-11-12 13:09:52 -0500

    refactor(dto) change DTO classes for records instead to avoid verbosity

--- backend/src/main/java/com/ecommerce/app/application/dto/request/MetodoPagoRequest.java
+++ backend/src/main/java/com/ecommerce/app/application/dto/request/MetodoPagoRequest.java
@@ -1,30 +1,6 @@
 package com.ecommerce.app.application.dto.request;
 
-public class MetodoPagoRequest {
-    private int id;
-    private String nombre;
-
-    public MetodoPagoRequest() {
-    }
-
-    public MetodoPagoRequest(int id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-}
+public record MetodoPagoRequest (
+        Integer id,
+        String nombre
+) {}
