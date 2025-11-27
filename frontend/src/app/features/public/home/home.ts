cfb2202d9a2a042f85a81e186a1a62007b23f96b
Paulo Armey <pauloarmey18@gmail.com>
2025-11-27 18:38:46 -0500

    feat(home): agregar componente de encabezado en la página de inicio

--- frontend/src/app/features/public/home/home.ts
+++ frontend/src/app/features/public/home/home.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ProductoService } from '../../../features/admin/productos/services/producto.service';
 import { Producto } from '../../../features/admin/productos/interfaces/producto';
+import { Header } from '../../../shared/components/header/header';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Header],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
