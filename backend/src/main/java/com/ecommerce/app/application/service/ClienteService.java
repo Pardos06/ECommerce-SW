6249b172027a4d03918d445b9bc7e64ef2b24c5b
Roddorr2 <rodricu0703@gmail.com>
2025-11-27 02:50:01 -0500

    refactor(cliente): revert changes made on WIP

--- backend/src/main/java/com/ecommerce/app/application/service/ClienteService.java
+++ backend/src/main/java/com/ecommerce/app/application/service/ClienteService.java
@@ -28,7 +28,6 @@ public class ClienteService {
     public List<ClienteResponse> listarClientes() {
         return clienteRepository.findAll()
                 .stream()
-                .filter(cliente -> "Cliente".equals(cliente.getUsuario().getNombre()))
                 .map(ClienteMapper::toResponse)
                 .collect(Collectors.toList());
     }
